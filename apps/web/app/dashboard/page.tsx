39227d044663688304f236ab5114861493229142
Jesper Hodge <jhodge@outlook.de>
2023-09-22 21:27:28 -0400

    switch to a google maps lib

---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, {FunctionComponent, useState, useCallback} from 'react';
+import {GoogleMapsProvider} from '@ubilabs/google-maps-react-hooks';
+
+import MapCanvas from '../../components/MapCanvas';
+import PlacesAutocompleteService from '../../components/PlacesAutocompleteService';
+
+const mapOptions = {
+  center: {lat: 53.5582447, lng: 9.647645},
+  zoom: 6,
+  disableDefaultUI: true,
+  zoomControl: false
+};
+
+const Dashboard: FunctionComponent<Record<string, unknown>> = () => {
+  const [mapContainer, setMapContainer] = useState<HTMLDivElement | null>(null);
+
+  const mapRef = useCallback(
+    (node: React.SetStateAction<HTMLDivElement | null>) => {
+      node && setMapContainer(node);
+    },
+    []
+  );
+
+  return (
+    <GoogleMapsProvider
+      googleMapsAPIKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
+      mapContainer={mapContainer}
+      mapOptions={mapOptions}
+      // Add the places library
+      libraries={['places']}>
+      <React.StrictMode>
+        <div id="container">
+          <MapCanvas ref={mapRef} />
+          <PlacesAutocompleteService />
+        </div>
+      </React.StrictMode>
+    </GoogleMapsProvider>
+  );
+};
+
+export default Dashboard;
--- apps/web/components/map.tsx
+++ apps/web/components/map.tsx
@@ -1,163 +1,32 @@
 'use client';
 
-import React, { useState, useRef } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import React, { useState, useRef, forwardRef, RefObject, ForwardedRef } from "react";
+import { useGoogleMap } from "@ubilabs/google-maps-react-hooks";
 
-var map: any;
-var service;
-var infowindow: any;
+type Ref = HTMLDivElement;
+interface Props {};
 
-
-function createMarker(place: google.maps.places.PlaceResult) {
-  if (!place.geometry || !place.geometry.location) return;
-
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, 'click', () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map, marker);
-  });
-}
-
-const loadMap = async () => {
-  const loader = new Loader({
-    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-    libraries: ["places"],
-    version: "weekly",
-  });
-
-  await loader.load()
-
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
-
-  infowindow = new google.maps.InfoWindow();
-
-  map = new google.maps.Map(
-      document.getElementById('map') as HTMLElement, {center: sydney, zoom: 15}
-    );
-
-  var request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
-  };
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      if (!results) return;
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results?.[i]);
-      }
-      map.setCenter(results[0]?.geometry?.location);
-    }
-  });
-
-  return map;
-};
-
-const Map = () => {
-  const [places, setPlaces] = useState<google.maps.places.PlaceResult[] | null | undefined>([]);
-  const [selectedPlace, setSelectedPlace] = useState<google.maps.places.PlaceResult | null>(null);
-
-  const mapRef = useRef<google.maps.Map | null>(null);
-  const searchBoxRef = useRef<google.maps.places.SearchBox | null>(null);
-  const markersRef = useRef<google.maps.Marker[] | null>([]);
-  const infowindowRef = useRef<google.maps.InfoWindow | null>(null);
-
-  const onPlacesChanged = () => {
-    const selectedPlaces = searchBoxRef.current!.getPlaces();
-    setPlaces(selectedPlaces);
-  
-    // Clear out old markers
-    markersRef.current?.forEach(marker => marker.setMap(null));
-    markersRef.current = [];
-  
-    const bounds = new google.maps.LatLngBounds();
-  
-    selectedPlaces?.forEach(place => {
-      if (!place?.geometry?.location) return;
-  
-      const marker = new google.maps.Marker({
-        map: mapRef.current,
-        title: place.name,
-        position: place.geometry.location
-      });
-      markersRef.current?.push(marker);
-  
-      google.maps.event.addListener(marker, 'click', () => {
-        setSelectedPlace(place);
-        infowindowRef.current!.setContent(place.name || "");
-        infowindowRef.current!.open(mapRef.current, marker);
-      });
-  
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-  
-    mapRef.current?.fitBounds(bounds);
-  };
-  
-  React.useEffect(() => {
-    async function mapEffect() {
-      // Load map...
-      const map = await loadMap();
-      
-      const initMapRefs = () => {
-        mapRef.current = map;
-        searchBoxRef.current = new google.maps.places.SearchBox(document.getElementById('search-bar') as HTMLInputElement);
-        infowindowRef.current = new google.maps.InfoWindow();
-      }
-      initMapRefs();
-
-      if (mapRef.current) {
-        mapRef.current.addListener('click', (event: any) => {
-          // Get latLng of the clicked location
-          const latLng = event.latLng;
-          // Do something with the clicked location if needed
-        });
-      }
-    
-      if (mapRef.current && searchBoxRef.current) {
-        const onPlacesChanged = () => {
-          const places = searchBoxRef.current!.getPlaces() || [];
-          setPlaces(places);
-          // ... handle selection
-        }
-    
-        searchBoxRef.current.addListener('places_changed', onPlacesChanged);
-    
-        return () => { // Cleanup
-          google.maps.event.clearListeners(searchBoxRef.current!, 'places_changed');
-        };
-      }
-    }
-
-    mapEffect()
-  }, []);
+const Map = forwardRef<Ref, Props>((props, ref) => {
+  const map = useGoogleMap();
+  const [selectedPlace, setSelectedPlace] = useState<google.maps.places.PlaceResult | null>(null)
 
   return (
     <div>
       <h3>Map</h3>
+      <div ref={ref} style={{ height: '80vh', width: '100%' }}></div>
       <input type="text" id="search-bar" placeholder="Search for places..." />
       <div id="search-results">
-      {selectedPlace && (
-        <div>
-          <h4>{selectedPlace.name}</h4>
-          <p>{selectedPlace.formatted_address}</p>
-          {/* You can add more details here */}
-        </div>
-      )}
-    </div>
-      <div id="map" style={{ height: '80vh', width: '100%' }}></div>
+        {selectedPlace && (
+          <div>
+            <h4>{selectedPlace?.name}</h4>
+            <p>{selectedPlace?.formatted_address}</p>
+            {/* You can add more details here */}
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+});
+Map.displayName = 'Map';
 
 export default Map;
