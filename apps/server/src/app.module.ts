104bec3f3c7ea2b2ecffa69bc5f4ad54976d5748
Jesper Hodge <jhodge@outlook.de>
2023-10-01 19:44:03 -0400

    feat: add prisma and postgres

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GathersModule } from './gathers/gathers.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GathersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), GathersModule],
   controllers: [AppController],
   providers: [AppService],
 })
