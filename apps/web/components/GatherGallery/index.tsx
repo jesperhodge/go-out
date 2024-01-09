90cd09401dc283a28dd623e5e311beb1f647eac5
Jesper Hodge <jhodge@outlook.de>
2024-01-08 21:04:17 -0500

    feat: add description and picture to event gallery

--- apps/web/components/GatherGallery/index.tsx
+++ apps/web/components/GatherGallery/index.tsx
@@ -15,17 +15,18 @@ export const GatherGallery: FC<Props> = ({ gatherList }) => {
         {gatherList?.map((gather) => {
           return (
             <div key={gather.id} className="w-64 h-64 border border-slate-400 mb-8">
-              <Image
-                src="https://loremflickr.com/256/144"
+              <img
+                src={gather.pictures?.[0] || 'https://loremflickr.com/256/144'}
                 alt="Gather Image"
-                className="object-contain"
+                className="object-contain max-h-32 w-full bg-black"
                 width={256}
                 height={144}
               />
-              <h4 className="text-lg text-slate-900 mb-8">{gather.name}</h4>
-              <p className="text-slate-900 break-all">
+              <h4 className="text-xl text-slate-900 mb-4 mt-4">{gather.name}</h4>
+              <p className="text-slate-900 text-sm break-all truncate max-w-100 mb-2">
                 {gather?.googlePlace?.formatted_address || 'no address'}
               </p>
+              <p className="text-slate-900 text-sm break-all overflow-ellipsis">{gather?.description}</p>
             </div>
           )
         })}
